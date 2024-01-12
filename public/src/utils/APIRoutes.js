d5cfa12a9489a0f0fc55031ba8529d73067ec2d6
flammmy <abhishp23@gmail.com>
2024-01-12 21:30:37 +0530

    updated routes

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,10 +1,8 @@
-export const host = "http://localhost:5000"
+export const host = "flamily-chat-app.vercel.app"
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
 export const allUsersRoutes =`${host}/api/auth/allusers`;
 export const sendMessagesRoute =`${host}/api/messages/addmsg`;
 export const getAllMessagesRoute =`${host}/api/messages/getmsg`;
-
-
 
